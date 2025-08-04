725a0b83858ab4fb49996203da123ae2513435b9
mrmo4567890 <amirmouris.marlina@gmail.com>
2025-08-04 19:41:15 +0300

    finish late tasks

---
+++ api_project/api/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path,include
+from .views import BookList, BookViewSet
+from rest_framework.routers import DefaultRouter
+from rest_framework.authtoken.views import obtain_auth_token
+
+router = DefaultRouter()
+router.register(r'my-models', BookViewSet)
+
+urlpatterns = [
+    path('books/', BookList.as_view(), name='book-list'),
+    path('', include(router.urls)),
+    path('api/token/', obtain_auth_token, name='api-token'),
+]
