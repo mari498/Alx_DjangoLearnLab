5901a396e62a6ab6615e0d6f9d135af63ee52244
mrmo4567890 <amirmouris.marlina@gmail.com>
2025-09-05 17:49:08 +0300

    First commit - Django API project

--- advanced_api_project/settings.py
+++ advanced_api_project/settings.py
@@ -31,13 +31,20 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+
+    # Third-party apps
+    "rest_framework",   # Django REST Framework
+
+    # Local apps
+    "api",              # Your API app
 ]
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
